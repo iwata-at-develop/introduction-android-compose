330eed545d87ce75ea8b058e42506fd6f6e09996
Iwata Atsushi <atsushi.iwata@systemi.co.jp>
2023-08-12 22:23:45 +0900

    Lesson 3 Material Design: Dark Theme.
    - Preview dark theme.

--- ComposeTutorial/app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ ComposeTutorial/app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.composetutorial
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -82,7 +83,12 @@ fun MessageCard(msg: Message) {
     }
 }
 
-@Preview(showBackground = true)
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
 @Composable
 fun PreviewMessageCard() {
     ComposeTutorialTheme {
