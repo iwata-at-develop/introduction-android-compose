c723da957da9539a00502fde5dbfd2f79258445c
Iwata Atsushi <atsushi.iwata@systemi.co.jp>
2023-08-13 20:58:03 +0900

    Implement a search feild in Jetpack Compose

--- BestWayImplementSearch/app/src/main/java/com/example/bestwayimplementsearch/MainActivity.kt
+++ BestWayImplementSearch/app/src/main/java/com/example/bestwayimplementsearch/MainActivity.kt
@@ -3,13 +3,25 @@ package com.example.bestwayimplementsearch
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.bestwayimplementsearch.ui.theme.BestWayImplementSearchTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +29,53 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             BestWayImplementSearchTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    SearchScreen()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun SearchScreen(
+) {
+    // searchText、persons の２つの変数が UI状態
+    // StateFlowで監視している為、値が変更されるとSearchScreenが 再Composeされる
+    val viewModel = viewModel<MainViewModel>()
+    val searchText by viewModel.searchText.collectAsState()
+    val persons by viewModel.persons.collectAsState()
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BestWayImplementSearchTheme {
-        Greeting("Android")
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        TextField(
+            value = searchText,
+            onValueChange = viewModel::onSearchTextChange,  // イベントを viewModelへ伝搬する
+            modifier = Modifier.fillMaxWidth(),
+            placeholder = { Text(text = "Search") }
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+        ) {
+            items(persons) { person ->
+                Text(
+                    text = "${person.firstName} ${person.lastName}",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 16.dp)
+                )
+            }
+        }
     }
 }
---
+++ BestWayImplementSearch/app/src/main/java/com/example/bestwayimplementsearch/MainViewModel.kt
@@ -0,0 +1,77 @@
+package com.example.bestwayimplementsearch
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
+
+class MainViewModel : ViewModel() {
+
+    /*
+     * asStateFlow を使って、MutableStateFlowからStateFlowへ変換しているのは、
+     * 値の変更は ViewModel内部で留めて、値の変更が出来ないStateFlowに変更したものを外部へ公開するテクニック
+     */
+
+    private val _searchText = MutableStateFlow("")
+    val searchText = _searchText.asStateFlow()
+
+    private val _persons = MutableStateFlow(allPersons)
+    val persons = searchText
+        .combine(_persons) { text, persons ->
+            if(text.isBlank()) {
+                persons
+            } else {
+                persons.filter {
+                    it.doesMatchSearchQuery(text)
+                }
+            }
+        }
+        .stateIn(
+            viewModelScope,
+            SharingStarted.WhileSubscribed(5000),
+            _persons.value
+        )
+
+    fun onSearchTextChange(text: String) {
+        _searchText.value = text
+    }
+}
+
+data class Person(
+    val firstName: String,
+    val lastName: String
+) {
+    fun doesMatchSearchQuery(query: String): Boolean {
+        val matchingCombinations = listOf(
+            "$firstName$lastName",
+            "$firstName $lastName",
+            "${firstName.first()} ${lastName.first()}",
+        )
+
+        return matchingCombinations.any {
+            it.contains(query, ignoreCase = true)
+        }
+    }
+}
+
+private val allPersons = listOf(
+    Person(
+        firstName = "Philipp",
+        lastName = "Lackner"
+    ),
+    Person(
+        firstName = "Beff",
+        lastName = "Jezos"
+    ),
+    Person(
+        firstName = "Chris P.",
+        lastName = "Bacon"
+    ),
+    Person(
+        firstName = "Jeve",
+        lastName = "Stops"
+    ),
+)
