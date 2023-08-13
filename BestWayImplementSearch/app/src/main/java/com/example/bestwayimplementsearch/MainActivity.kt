aa77738ed79c3ed5b8a08d5039d65956013da42a
Iwata Atsushi <atsushi.iwata@systemi.co.jp>
2023-08-13 21:39:34 +0900

    Show progress bar during search.

--- BestWayImplementSearch/app/src/main/java/com/example/bestwayimplementsearch/MainActivity.kt
+++ BestWayImplementSearch/app/src/main/java/com/example/bestwayimplementsearch/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.bestwayimplementsearch
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,6 +12,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -19,6 +21,7 @@ import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -49,6 +52,7 @@ fun SearchScreen(
     val viewModel = viewModel<MainViewModel>()
     val searchText by viewModel.searchText.collectAsState()
     val persons by viewModel.persons.collectAsState()
+    val isSearching by viewModel.isSearching.collectAsState()
 
     Column(
         modifier = Modifier
@@ -62,19 +66,26 @@ fun SearchScreen(
             placeholder = { Text(text = "Search") }
         )
         Spacer(modifier = Modifier.height(16.dp))
-
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxWidth()
-                .weight(1f)
-        ) {
-            items(persons) { person ->
-                Text(
-                    text = "${person.firstName} ${person.lastName}",
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(vertical = 16.dp)
+        if(isSearching) {
+            Box(modifier = Modifier.fillMaxSize()) {
+                CircularProgressIndicator(
+                    modifier = Modifier.align(Alignment.Center)
                 )
+            }
+        } else {
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+            ) {
+                items(persons) { person ->
+                    Text(
+                        text = "${person.firstName} ${person.lastName}",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 16.dp)
+                    )
+                }
             }
         }
     }
--- BestWayImplementSearch/app/src/main/java/com/example/bestwayimplementsearch/MainViewModel.kt
+++ BestWayImplementSearch/app/src/main/java/com/example/bestwayimplementsearch/MainViewModel.kt
@@ -1,14 +1,21 @@
 package com.example.bestwayimplementsearch
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
 
 class MainViewModel : ViewModel() {
+
+    private val TAG: String = "MainViewModel"
 
     /*
      * asStateFlow を使って、MutableStateFlowからStateFlowへ変換しているのは、
@@ -18,17 +25,25 @@ class MainViewModel : ViewModel() {
     private val _searchText = MutableStateFlow("")
     val searchText = _searchText.asStateFlow()
 
+    private val _isSearching = MutableStateFlow(false)
+    val isSearching = _isSearching.asStateFlow()
+
     private val _persons = MutableStateFlow(allPersons)
     val persons = searchText
+        .debounce(500L) // 0.5秒経過してから最新の値で次のイベントが発生
+        .onEach { _isSearching.update { true } }
         .combine(_persons) { text, persons ->
+            Log.e(TAG, "searchText is ${searchText.value}")
             if(text.isBlank()) {
                 persons
             } else {
+                delay(1000L) // StrageやNetworkからデータを取得することを想定
                 persons.filter {
                     it.doesMatchSearchQuery(text)
                 }
             }
         }
+        .onEach { _isSearching.update { false } }
         .stateIn(
             viewModelScope,
             SharingStarted.WhileSubscribed(5000),
